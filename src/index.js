3b4dbe2468f07648f9ef3c6bf3d962aad34a80fe
officialrahulgoyal26 <officialrahulgoyal26@gmail.com>
2021-06-03 16:56:30 +0530

    my fav movies name

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <>
+  <h1><center>MY FAV MOVIES NAME</center></h1>
+  <p><center>LOOK LEFT</center></p>
+  <ol>
+    <li>3 idiots</li>
+    <li>chichore</li>
+    <li>lucifer</li>
+    <li>ABCD</li>
+    <li>i dont know</li>
+  </ol>
+  <h2><center>HOPE YOU LIKE MY MOVIES NAME</center></h2>
+  </>,
+  document.getElementById("root")
+  
+);
